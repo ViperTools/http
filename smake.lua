37d9755472fe83cde281ae89e816980c00b7ce34
ViperTools <cboatyt@gmail.com>
2023-12-11 15:21:38 -0600

    Update Smake file

--- smake.lua
+++ smake.lua
@@ -1,5 +1,5 @@
 local import = import('smake/libraryInstaller')
-import('smake/gpp', true)
+local gppCompiler = import('smake/gpp')
 import('smake/dependencyInstaller', true)
 import('smake/dependencyIncluder', true)
 
@@ -8,12 +8,13 @@ function smake.install()
 end
 
 function smake.build()
+    gppCompiler():makeGlobal()
     standard('c++2a')
     flags('-c')
     include('include')
     IncludeDependency('curl')
     input('src/*.cpp')
-    build()
+    compile()
 
     run('ar rvs libhttp.a http_request.o http_response.o http.o')
     run('rm *.o')
